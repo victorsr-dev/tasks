29d452f45ff52e18bcd5d4c4ebc97a6afdfa1963
Victor Ruiz <vik_ases@live.com.mx>
2020-02-05 20:28:50 -0600

    Create Login Component

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,12 @@
 <template>
   <div id="app">
-    <!--div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/-->
-    <section class="hero is-primary is-fullheight">
-      <div class="hero-body">
-        <div class="container">
-          <div class="columns is-centered">
-            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
-              <form action class="box" v-on:submit.prevent >
-                <div class="field">
-                  <label for="email" class="label">Email</label>
-                  <div class="control has-icons-left">
-                    <input
-                      id="email"
-                      type="email"
-                      placeholder="e.g. bobsmith@gmail.com"
-                      class="input"
-                      required
-                      v-model="user.email"
-                    />
-                    <span class="icon is-small is-left">
-                      <font-awesome-icon icon="envelope"/>
-                    </span>
-                  </div>
-                </div>
-                <div class="field">
-                  <label for="password" class="label">Password</label>
-                  <div class="control has-icons-left">
-                    <input
-                      id="password"
-                      type="password"
-                      placeholder="*******"
-                      class="input"
-                      required
-                      v-model="user.password"/>
-                    <span class="icon is-small is-left">
-                      <font-awesome-icon icon="lock"/>
-                    </span>
-                  </div>
-                </div>
-                <div class="field">
-                  <button class="button is-success is-fullwidth" @click="login()">Login</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Login/>
   </div>
 </template>
 <script>
-import LoginService from '@/services/login'
+import Login from '@/components/Login'
 export default {
-  data () {
-    return {
-      user: {
-        email: '',
-        password: ''
-      }
-    }
-  },
-  methods: {
-    async login () {
-      try {
-        const res = await LoginService.getUser(this.user)
-        if (res.data.success) {
-          this.$store.dispatch('setUser', res.data.user)
-          this.$swal('Bienvenido...!!')
-        } else {
-          this.$swal('Hola Mundo')
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
+  components: { Login }
 }
 </script>
 <style lang="scss">
---
+++ src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+<section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form action class="box" v-on:submit.prevent >
+                <div class="field">
+                  <label for="email" class="label">Email</label>
+                  <div class="control has-icons-left">
+                    <input
+                      id="email"
+                      type="email"
+                      placeholder="e.g. bobsmith@gmail.com"
+                      class="input"
+                      required
+                      v-model="user.email"
+                    />
+                    <span class="icon is-small is-left">
+                      <font-awesome-icon icon="envelope"/>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <label for="password" class="label">Password</label>
+                  <div class="control has-icons-left">
+                    <input
+                      id="password"
+                      type="password"
+                      placeholder="*******"
+                      class="input"
+                      required
+                      v-model="user.password"/>
+                    <span class="icon is-small is-left">
+                      <font-awesome-icon icon="lock"/>
+                    </span>
+                  </div>
+                </div>
+                <div class="field">
+                  <button class="button is-success is-fullwidth" @click="login()">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+<script>
+import LoginService from '@/services/login'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      this.$swal('Bienvenido...!!')
+      try {
+        const res = await LoginService.getUser(this.user)
+        if (res.data.success) {
+          this.$store.dispatch('setUser', res.data.user)
+          this.$swal('Bienvenido...!!')
+        } else {
+          this.$swal('Hola Mundo')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
